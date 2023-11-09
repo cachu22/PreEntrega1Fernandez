e2845112df65d596ee8f5082c7e543dfc84f8b7f
cachu22 <117787700+cachu22@users.noreply.github.com>
2023-11-08 22:46:55 -0300

    segundaentrega

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
-import { getProducts, getProductsByCategory} from '../../asyncMock'
-import ItemList from '../ItemList/ItemList'
+/* import { useState, useEffect } from "react"; */
+/* import { getProducts, getProductsByCategory} from '../../asyncMock' */
+/* import ItemList from '../ItemList/ItemList' */
 
 
 
